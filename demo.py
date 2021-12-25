b09e4fd874fa9464d4f1812746eebd2febb189d3
Vyacheslav <baltamos@yandex.ru>
2021-12-26 00:04:09 +0300

    -initial

--- demo.py
+++ demo.py
@@ -7,18 +7,23 @@ from dash.dependencies import Input, Output, State
 import dash_bootstrap_components as dbc
 from tensorflow.keras import models
 
-from graphs import dg_graph, temp_graph, dg_graph_mini, temp_graph_mini
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 
 from sdk import SedmaxHeader
-#assets_path = ''
+
 
 # Create a dash application
-app = dash.Dash(__name__, external_stylesheets=[dbc.themes.DARKLY]) #, assets_folder=assets_path
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.MATERIA]) #, assets_folder=assets_path
 
 normal_path = './data/normal/'
 imbalanced_path = './data/imbalanced/'
-normal_cases = os.listdir(normal_path)
-imbalanced_cases = os.listdir(imbalanced_path)
+misalignment_path = './data/misalignment/'
+ball_path = './data/ball_fault/'
+cage_path = './data/cage_fault/'
+outer_path = './data/outer_fault/'
+
+
 
 model = models.load_model('model/')
 
@@ -27,71 +32,149 @@ def load_data(path):
     data = np.array(data[4]).reshape(-1, 1000)
     return data
 
+def plot_data(true, pred, label):
+
+    errors = abs(pd.DataFrame(true) - pd.DataFrame(pred)).mean()
+    true = np.array(true).flatten()
+    pred = np.array(pred).flatten()
+
+    statistics = {'STD': errors.std(),
+                  'Skewness': errors.skew(),
+                  'IQR': errors.quantile(.75) - errors.quantile(.25),
+                  'Mean': errors.mean(),
+                  'Median': errors.median()
+                  }
+
+    fig = make_subplots(rows=2, cols=2, shared_xaxes=True, vertical_spacing=0.07, row_width=[0.2, 0.3],
+                        specs=[[{"colspan": 2}, None], [{}, {}]], )
+    fig.add_trace(go.Scatter(y=true, mode='lines', line=dict(color='steelblue', width=2), name='Показания'),
+                  row=1, col=1)
+    fig.add_trace(go.Scatter(y=pred, mode='lines', line=dict(color='salmon', width=2), name='Выходные данные'),
+                  row=1, col=1)
+
+    fig.add_trace(go.Histogram(x=errors, name='Распределение ошибок'),
+                  row=2, col=1),
+    fig.add_trace(
+        go.Bar(y=list(statistics), x=list(statistics.values()), name='Стат. показатели ошибок', orientation='h'),
+        row=2, col=2)
+
+    fig.update_layout(height=900,
+                      paper_bgcolor="rgba(100,149,237,0.05)",
+                      title_text=label,
+                      title={'font': {'size': 20}},
+                      title_y=0.98,
+                      title_x=0.5,
+                      hovermode="x unified",
+                      #plot_bgcolor="rgba(100,149,237,0.5)",
+                      legend=dict(orientation="h", yanchor="bottom", y=1.02, xanchor="right", x=1),
+                      margin=dict(l=20, r=20, b=30, t=100, pad=1),
+                      )
+
+
+    return fig
+
 
 # Create an app layout
 body = dbc.Container([
-        dbc.Row([ dcc.Graph(id='normal_engine', animate=True)
-        ]),
-        dbc.Row([ dcc.Graph(id='fault_engine', animate=True)
-        ]),
-html.Div([
-    html.Button('Исправный', id='btn1', n_clicks=0),
-    html.Button('Несправный', id='btn2', n_clicks=0),
-])
-    ], fluid=True, style = {'background-color': '#5f8dab'})
+        html.Br(),
+        dbc.Row([
+            dbc.Col([dcc.Graph(id='normal_engine', animate=False),
+            html.Button('Исправный', className="btn btn-lg btn-secondary", id='btn1', n_clicks=0,
+                        style={'margin': '14px', 'background-color': "rgba(100,149,237,0.1))", 'box-shadow': '0 0 5px rgba(0,0,0,0.5)'}),
+            ]),
+            dbc.Col([dcc.Graph(id='fault_engine', animate=False),
+            html.Button('Неисправный', className="btn btn-lg btn-secondary", id='btn2', n_clicks=0,
+                        style={'margin': '14px', 'background-color': "rgba(100,149,237,0.1))", 'box-shadow': '0 0 5px rgba(0,0,0,0.5)'}),
+            ]),
+        ], style = {'display':'flex', 'flex-flow': 'row nowrap'}),
+
+        html.Br(),
+        dcc.Link(html.Button('Подробнее о данных', className="btn btn-sm btn-secondary", id="collapse-button",
+                 style={'margin': '14px', 'background-color': "rgba(100,149,237,0.1))", 'box-shadow': '0 0 5px rgba(0,0,0,0.5)'}),
+                 href='http://www02.smt.ufrj.br/~offshore/mfs/page_01.html'),
+
+        dbc.Collapse(
+            dbc.Card(dbc.CardBody([html.Div(html.Img(src='assets/image.png', style={'height': '100%', 'width': '100%'})),
+                                html.Div(html.Img(src='assets/bearing.png',
+                                style={'height': '30%', 'width': '30%',
+                                        'margin-left': 'auto', 'margin-right': 'auto', 'display':'block'}))]
+                                  )),
+            id="collapse",
+            is_open=False,
+        ),
+
+    ], fluid=True, style = {'background-color': '#fff' }) #'#5f8dab'
 
 
 app.layout = html.Div(children=[
-    SedmaxHeader(title='Анализ вибрации синхронного двигателя'),
+    dcc.Store(id='memory_normal'),
+    dcc.Store(id='memory_fault'),
+    html.Div(
+    SedmaxHeader(title='Анализ вибрации синхронного двигателя', color='#323232'),
+    style={'box-shadow': '0 0 10px rgba(0,0,0,0.5)', 'display': 'grid'}),
     body,
+
 ])
 
 
-@app.callback(Output(component_id='memory-output', component_property='data'),
-              [Input(component_id='interval-component', component_property='n_intervals'),
-               Input(component_id='slider_1', component_property='value'),
-               Input(component_id='slider_2', component_property='value')]
+@app.callback(Output(component_id='memory_normal', component_property='data'),
+              Input(component_id='btn1', component_property='n_clicks'))
 
-              )
-def update_global_var(n, days, minutes):
-   return load_data_new(days, minutes)
-
-@app.callback(
-    [Output(component_id='normal_engine', component_property='figure')],
-    [Input(component_id='btn1', component_property='n_clicks')]
- )
-def normal_chart(n):
-
+def normal_data(n):
     path = normal_path + np.random.choice(os.listdir(normal_path))
     data = load_data(path)
     pred = model.predict(data)
+    label = 'Normal'
+    return [data, pred, label]
 
-    #вставать график
+@app.callback(Output(component_id='memory_fault', component_property='data'),
+              Input(component_id='btn2', component_property='n_clicks'))
 
-    return fig1
+
+def fault_data(n):
+    path = np.random.choice([imbalanced_path, misalignment_path, ball_path, cage_path, outer_path])
+    if path == imbalanced_path:
+        label = 'Imbalanced'
+    elif path == misalignment_path:
+        label = 'Misalignment'
+    elif path == ball_path:
+        label = 'Ball Fault'
+    elif path == cage_path:
+        label = 'Cage Fault'
+    elif path == outer_path:
+        label = 'Outer Fault'
+
+    path = path + np.random.choice(os.listdir(path))
+    data = load_data(path)
+    pred = model.predict(data)
+    return [data, pred, label]
+
+
+@app.callback([Output(component_id='normal_engine', component_property='figure'),
+               Output(component_id='fault_engine', component_property='figure')],
+              [Input(component_id='memory_normal', component_property='data'),
+               Input(component_id='memory_fault', component_property='data')]
+              )
+
+def update_chart(normal_data, fault_data):
+
+
+    fig1 = plot_data(normal_data[0], normal_data[1], normal_data[2])
+    fig2 = plot_data(fault_data[0], fault_data[1], fault_data[2])
+
+    return [fig1, fig2]
+
 
 @app.callback(
-    [Output(component_id='fault_engine', component_property='figure')],
-    [Input(component_id='btn2', component_property='n_clicks')]
- )
-def fault_chart(n):
-    el_archive = pd.read_json(data[0])
-    rtd_archive = pd.read_json(data[1])
-    rtd = pd.read_json(data[2])
-
-    fig1 = dg_graph([el_archive, rtd_archive], n)
-    fig2 = temp_graph(rtd_archive, n)
-    fig3 = dg_graph_mini(rtd)
-    fig4 = temp_graph_mini(rtd)
-
-    return fig1
-
-@app.callback(
-    Output("offcanvas", "is_open"),
-    Input("open-offcanvas", "n_clicks"),
-    [State("offcanvas", "is_open")],
+    Output("collapse", "is_open"),
+    [Input("collapse-button", "n_clicks")],
+    [State("collapse", "is_open")],
 )
-def toggle_offcanvas(n1, is_open):
-    if n1:
+def toggle_collapse(n, is_open):
+    if n:
         return not is_open
     return is_open
+
+
+if __name__ == "__main__":
+    app.run_server(debug=True)
